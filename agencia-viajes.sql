7f98bc11811efd1b716b12624e84d2d575e4f5ae
saenz-arias <harry.saenz1@gmail.com>
2022-05-30 13:39:59 -0500

    cuarto commit

--- agencia-viajes.sql
+++ agencia-viajes.sql
@@ -28,7 +28,8 @@ CREATE TABLE `testimoniales` (
   `correo` varchar(60) NOT NULL,
   `mensaje` text NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+-- ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
